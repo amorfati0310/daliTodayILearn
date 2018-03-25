cd31a8be86d2798c8734296eb552d34dc60e619d
chany <lyhy0310@gmail.com>
2018-03-25 23:48:10 +0900

    daily algorithm update

--- algorithm/codility/codility03/codility03.03.js
+++ algorithm/codility/codility03/codility03.03.js
@@ -10,4 +10,15 @@ function solution(A) {
 }
 console.log(solution([1, 2, 3, 4]));
 
+
+// 다른 사람 파이썬 풀이 -> sum을 구한 다음에 하나씩 뺴준다 !!!
+// def solution(A):
+//     N = len(A) + 1
+//     missing = ((N + 1) * N) / 2
+
+//     for x in A:
+//         missing -= x
+
+//     return missing
+
 // O(N) or O(N * log(N))
